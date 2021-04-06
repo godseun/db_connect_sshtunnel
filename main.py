f1d0482138b643f55f86c7fd87b0a02a5d68d94e
godseun <alcy1024@gmail.com>
2021-04-06 18:10:16 +0900

    add ref

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ from mysql import connector as db
 config = Config()
 
 def query(q):
+    """
+    config:
+    ssh_host = 'target_addr'            ex. 3.33.22.222
+    ssh_port = target_port              ex. 22
+    ssh_username = 'target_username'    ex. ubuntu
+    ssh_pkey='auth_key_path'            ex. /home/user/aws.pem
+    remote_host = 'remote_addr'         ex. 127.0.0.1
+    remote_port = remote_port           ex. 3306
+    db_host = 'db_addr'                 ex. 127.0.0.1
+    db_user = 'user'                    ex. root
+    db_password = 'password'            ex. 123123
+    db_name = 'schema_name'             ex. ssh_db
+    """
     with sshtunnel.SSHTunnelForwarder(
         ssh_address_or_host=(config.ssh_host, config.ssh_port),
         ssh_username=config.ssh_username,
